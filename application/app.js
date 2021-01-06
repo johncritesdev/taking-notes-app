8fb21fbebc5001596c36bdb0e373c61b1b0b6252
John Crites <johncritesdev@gmail.com>
2021-01-06 11:37:53 -0500

    ...

--- application/app.js
+++ application/app.js
@@ -1,5 +1,9 @@
+const validator = require('validator');
 const notes = require('./notes');
 
 const msg = notes();
 
 console.log(msg);
+
+console.log(validator.isEmail('john@test.com'));
+console.log(validator.isURL('https://johncritesdev.com'));
