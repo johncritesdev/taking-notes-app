74c33ce8b5c5c12d9b3c3ed119c3271584509539
John Crites <johncritesdev@gmail.com>
2021-01-07 19:24:43 -0500

    ...

--- application/app.js
+++ application/app.js
@@ -1,9 +1,10 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const notes = require('./notes');
 
 const msg = notes();
 
 console.log(msg);
 
-console.log(validator.isEmail('john@test.com'));
-console.log(validator.isURL('https://johncritesdev.com'));
+const note = chalk.blue.bold('hello world');
+
+console.log(note);
